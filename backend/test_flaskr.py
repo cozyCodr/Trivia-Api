74edd4e0d2514f0c10dd53db8598310221af5884
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-09-11 09:26:39 +0200

    written get_categories 405 error test

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -41,7 +41,13 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['categories'])
         self.assertEqual(data["success"], True)
 
-    
+    def test_405_method_no_allowed_get_categories(self):
+        res = self.client().post('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
